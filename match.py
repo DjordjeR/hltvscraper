f689c155a80ddba12c747166cbcdc4fc3a0fe7d8
Djordje Rajic <djordje.rajic@student.tugraz.at>
2019-05-28 13:19:11 +0200

    Added time

--- match.py
+++ match.py
@@ -1,9 +1,10 @@
 class Match:
     """Holds the data about signle match."""
 
-    def __init__(self, team1, team2):
+    def __init__(self, team1, team2, time):
         self.team2 = team2
         self.team1 = team1
+        self.time = time
 
     def __str__(self):
-        return self.team1 + " vs " + self.team2
+        return self.team1 + " vs " + self.team2 + " - " + self.time
--- scraper.py
+++ scraper.py
@@ -7,13 +7,14 @@ from match import Match
 
 
 def scraper():
-    url = "https://www.hltv.org/"
+    # url = "https://www.hltv.org/"
     matches = []
     # We need to use this to avoid getting flaged as bot.
     # worker = cfscrape.create_scraper()
     # response = worker.get(url).content
 
     f = open("hltv_page.txt", "r")
+    # I placed the page in a txt file so we do not spam the real page with request for now.
     response = f.read()
     f.close()
 
@@ -22,8 +23,11 @@ def scraper():
 
     for match in all_matches_raw:
         current_teams = match.findAll("span", {"class", "team"})
+        current_teams_time = match.findAll("div", {"class", "middleExtra"})
         if len(current_teams) == 2:
-            matches.append(Match(current_teams[0].get_text(), current_teams[1].get_text()))
+            matches.append(Match(current_teams[0].get_text(), current_teams[1].get_text(),
+                                 current_teams_time[0].get_text()))
+            # TODO: Get the start number of the game, get the flags of the game.
 
     for m in matches:
         print(m)
